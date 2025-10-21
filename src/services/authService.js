3f8c4a1b1a4a8f6d2e581a951f134d5b811ac316
Miguel Angel <136278328+AngelmarkX@users.noreply.github.com>
2025-10-20 22:11:10 -0500

    prueba2

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-const API_BASE_URL = "http://192.168.1.5:3006/api"
+const API_BASE_URL = "https://backend-production-b28f.up.railway.app/api"
 
 class AuthService {
   constructor() {
